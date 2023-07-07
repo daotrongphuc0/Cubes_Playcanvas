b88aa8429a74371e78acaf75c706d103b6ae0c08
daotrongphuc0 <phucdt504@gmail.com>
2023-07-07 14:53:27 +0700

    + add play screen

---
+++ src/Screen/playScreen.js
@@ -0,0 +1,21 @@
+import { Vec2, Vec4 } from "playcanvas";
+import { GameConstant } from "../GameConstant";
+import { UIScreen } from "../ui/UIScreen"
+import { Button } from "../Object/ui/button";
+
+export class PlayScreen extends UIScreen {
+  constructor() {
+    super(GameConstant.SCREEN_PLAY);
+    this._initButtonControls();
+  }
+
+  _initButtonControls() {
+    this.leftButton = new Button({
+      anchor: new Vec4(0.7, 0.15, 0.7, 0.15),
+      pivot: new Vec2(0.5, 0.5),
+      margin: new Vec4(),
+    });
+    this.addChild(this.leftButton);
+    this.leftButton.text.element.text = "Left";
+  }
+}

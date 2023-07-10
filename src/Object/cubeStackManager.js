a78a19b77973c3f3935e3c0f9dfa00377ad65091
daotrongphuc0 <phucdt504@gmail.com>
2023-07-10 15:28:25 +0700

    + add snake update tail before eat

--- src/Object/cubeStackManager.js
+++ src/Object/cubeStackManager.js
@@ -3,6 +3,8 @@ import { Spawner } from "../scripts/spawner";
 import { Cube } from "./cube";
 import { Time } from "../systems/time/time";
 import { Helper } from "../Helper/Helper";
+import { GameConstant } from "../GameConstant";
+import { update } from "@tweenjs/tween.js";
 
 
 export const CubeStackManagerEvent = Object.freeze({
@@ -80,6 +82,65 @@ export class CubeStackManager extends Entity {
       this.cubes[i].reset(delayTime + Helper.getScaleByNumber(this.cubes[i].number) + 0.005)
       delayTime += Helper.getScaleByNumber(this.cubes[i].number) + 0.005
     }
+
+    setTimeout(() => {
+      this.checkUpdateSnake()
+    }, 3000)
+
     return cube;
   }
+
+  checkUpdateSnake() {
+    var isUpdate = false
+    var x = 1;
+    if (this.player.number === this.cubes[0].number) {
+      this.player.levelUp()
+      this.cubes[0].destroy()
+      this.cubes.splice(0, 1)
+      let delayTime1 = 0
+      console.log("XXXX");
+      this.cubes[0].reset(0.1)
+      for (var i = 1; i < this.cubes.length; i++) {
+        this.cubes[i].reset(delayTime1 + Helper.getScaleByNumber(this.cubes[i].number) + 0.005)
+        delayTime1 += Helper.getScaleByNumber(this.cubes[i].number) + 0.005
+      }
+      x++
+      isUpdate = true
+    }
+    for (x; x < this.cubes.length; x++) {
+      if (this.cubes[x].number === this.cubes[x - 1].number) {
+        let delayTime = this.cubes[x - 1].mover.delayTime
+        this.cubes[x - 1].levelUp()
+        this.cubes[x].destroy()
+        this.cubes.splice(x, 1)
+
+        for (var i = x; i < this.cubes.length; i++) {
+          this.cubes[i].reset(delayTime + Helper.getScaleByNumber(this.cubes[i].number) + 0.005)
+          delayTime += Helper.getScaleByNumber(this.cubes[i].number) + 0.005
+        }
+        x += 2
+        isUpdate = true
+      }
+    }
+    if (isUpdate) {
+      setTimeout(() => {
+        this.checkUpdateSnake()
+      }, 3000)
+    }
+  }
+
+  // sortUpdate() {
+  //   console.log("sort");
+  //   this.cubes = this.sortArrayDescending(this.cubes)
+  //   var delay = 0;
+  //   this.cubes.forEach(element => {
+  //     delay += 0.1
+  //     element.reset(delay)
+  //   });
+  // }
+
+  // sortArrayDescending(arr) {
+  //   arr.sort((a, b) => b.number - a.number);
+  //   return arr;
+  // }
 }

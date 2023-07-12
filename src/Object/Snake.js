62857b679df72f498f83d4bbd760ff69e2712e46
daotrongphuc0 <phucdt504@gmail.com>
2023-07-12 14:22:06 +0700

    + add event colistion cube

--- src/Object/Snake.js
+++ src/Object/Snake.js
@@ -1,10 +1,11 @@
 import { Vec3 } from "playcanvas";
 import { Cube } from "./cube";
-import { PlayerMovement } from "../scripts/playerMove";
 import { GameConstant } from "../GameConstant";
 import { Helper } from "../Helper/Helper";
-import { SnakeMove } from "../scripts/snakeMove";
-import { TestMove } from "../scripts/testmove";
+import { SnakeMove } from "../scripts/moveObject/snakeMove";
+import { Wall } from "./Wall";
+import { Item } from "./Item";
+
 
 export class Snake extends Cube {
     constructor(name, num) {
@@ -18,6 +19,51 @@ export class Snake extends Cube {
             speed: GameConstant.PLAYER_SPEED,
         })
 
+        this.addComponent("rigidbody", {
+            type: "kinematic",
+        });
+
+        this.addComponent('collision', {
+            type: 'box',
+            halfExtents: new pc.Vec3(scale, scale, scale)
+        });
+
+        console.log(scale)
+        this.collision.on('collisionstart', (result) => {
+            console.log(result);
+            var otherEntity = result.other;
+
+            if (otherEntity instanceof Wall) {
+                console.log("Player va chạm với Wall");
+            }
+
+            if (otherEntity instanceof Item) {
+                console.log("Player va chạm với item");
+            }
+
+            if (otherEntity instanceof Cube) {
+                var isMyCube = false;
+                this.cubeStack.cubes.forEach(element => {
+                    if (element === otherEntity) {
+                        isMyCube = true;
+                    }
+                });
+                if (!isMyCube) {
+                    console.log("XXX");
+                }
+            }
+
+        })
+
+    }
+
+    setSpeed(speed) {
+        this.move.setSpeed(speed)
+        if (this.cubeStack) {
+            this.cubeStack.cubes.forEach(element => {
+                element.mover.setSpeed(speed)
+            });
+        }
     }
 
 }

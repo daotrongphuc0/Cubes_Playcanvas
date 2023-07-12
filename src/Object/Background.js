ee7ecda807c3e764e65f9e7727679a0ab98e42eb
daotrongphuc0 <phucdt504@gmail.com>
2023-07-12 14:19:04 +0700

    - draw background by json

--- src/Object/Background.js
+++ src/Object/Background.js
@@ -2,21 +2,20 @@ import * as pc from "playcanvas";
 import { AssetsLoader } from "../assets/AssetsLoader";
 
 export class Background extends pc.Entity {
-    constructor() {
+    constructor(x = 20, y = 20) {
         super("background")
 
         this.material = new pc.StandardMaterial()
         this.material.diffuse = new pc.Color(31 / 255, 54 / 255, 97 / 255)
         this.material.diffuseMap = AssetsLoader.getAssetByKey("dotmap").resource
-        this.material.diffuseMapTiling = new pc.Vec2(40, 40);
+        this.material.diffuseMapTiling = new pc.Vec2(x / 2, y / 2);
         this.material.update()
-
 
         this.addComponent("model", {
             type: "plane",
             material: this.material,
         });
-        this.setLocalScale(50, 0, 50)
+        this.setLocalScale(x, 0, y)
         this.setLocalPosition(0, 0, 0)
         this.setLocalEulerAngles(0, 0, 0)
     }

6362393ebac25120db32b04bb64c4a11213ea204
daotrongphuc0 <phucdt504@gmail.com>
2023-07-17 17:29:53 +0700

    - remove default not use

--- src/GameConstant.js
+++ src/GameConstant.js
@@ -17,6 +17,7 @@ export const GameConstant = Object.freeze({
     SCREEN_PLAY: "PlayScreen",
 
     PLAYER_SPEED: 2,
+    PLAYER_SPEED_UP: 4,
 
     DEFAULT_SCALE_BOX_MIN: 0.5,
     DEFAULT_SCALE_SIZE_INCREASE: 0.1,
@@ -25,14 +26,13 @@ export const GameConstant = Object.freeze({
 
     DEFAULT_LOCATION_CAMERA: new Vec3(-10, 25, 0),
 
-    DEFAULT_HEIGHT_WALL: 2,
-
+    DEFAULT_HEIGHT_WALL: 1.5,
     DEFAULT_COLOR_WALL: new Color(85 / 255, 85 / 255, 85 / 255),
-
-    SIZE_MODEL_BOX: 2,
 
     HEIGHT_ITEM: 0.01,
     SIZE_ITEM: 3,
 
     SIZE_WALL_AROUND: 5,
+
+    LIMIT_TIME_POS_QUEUE: 10
 });

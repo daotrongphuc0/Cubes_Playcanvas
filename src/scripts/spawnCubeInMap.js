26386d3c25449cdebd952fe80698792815faf43e
daotrongphuc0 <phucdt504@gmail.com>
2023-07-17 17:27:32 +0700

    + add func spawn cube in map

--- src/scripts/spawnCubeInMap.js
+++ src/scripts/spawnCubeInMap.js
@@ -1,6 +1,12 @@
 import { update } from "@tweenjs/tween.js";
 import { Script } from "../systems/script/script";
 import { Spawner } from "./spawner";
+import { Helper } from "../Helper/Helper";
+import data from "../../assets/json/datalv1.json";
+import { Cube } from "../Object/cube";
+import { Scene } from "playcanvas";
+import { SceneManager } from "../Scene/SceneManager";
+
 
 
 
@@ -15,6 +21,23 @@ export const SpawmCubeInMap = Script.createScript({
         }
     },
     spawn() {
+        var x = Helper.randomFloor(0, data.background.size[0])
+        var y = Helper.randomFloor(0, data.background.size[0])
+        var num = Helper.randomFloor(0, 4)
+        var cube = new Cube(num)
+        cube.setLocalPosition(x, 0, y)
+        SceneManager.currentScene.addChild(cube)
+        SceneManager.currentScene.cubes.push(cube)
+        if (!cube.enable) {
+            SceneManager.currentScene.removeChild(cube)
+            cube.destroy()
+            SceneManager.currentScene.cubes.pop()
+            this.spawn()
+        }
+
+        // cube.on('collisionstart', (result) => {
+        //     console.log("check ok");
+        // })
 
     }
 })

611bc59092cf6b25b3b416b606972b6a9b10cffc
daotrongphuc0 <phucdt504@gmail.com>
2023-07-19 10:46:22 +0700

    + add check collis snake numerical

--- src/scripts/moveObject/snakeMove.js
+++ src/scripts/moveObject/snakeMove.js
@@ -12,8 +12,10 @@ export const SnakeMove = Script.createScript({
     },
 
     _tmpPos: new Vec3(),
+    _tmpPos1: new Vec3(),
     vector: new Vec3(1, 0, 0),
     vectorDisable: new Vec3(0, 0, 0),
+    snakeColliston: null,
 
     update() {
         var direction = this.vector
@@ -29,20 +31,49 @@ export const SnakeMove = Script.createScript({
                 if (SceneManager.currentScene.wall[i].orientedBox.containsPoint(this._tmpPos)) {
                     this._tmpPos.x += xMovement;
                 } else {
+                    xMovement = 0
                     continue;
                 }
                 this._tmpPos.z -= zMovement;
                 if (SceneManager.currentScene.wall[i].orientedBox.containsPoint(this._tmpPos)) {
                     this._tmpPos.z += zMovement;
                 } else {
+                    zMovement = 0
                     continue;
                 }
                 this._tmpPos.x -= xMovement;
                 this._tmpPos.z -= zMovement;
+                xMovement = 0;
+                zMovement = 0;
                 break;
             }
         }
-        this.entity.setPosition(this._tmpPos);
+        this.entity.setPosition(this.checkCollistionSnake(this._tmpPos, xMovement, zMovement));
+    },
+
+    checkCollistionSnake(pos, xMovement, zMovement) {
+        if (!this.snakeColliston) {
+            return pos;
+        }
+
+        // console.log(xMovement + " " + zMovement);
+        // if (xMovement != 0 || zMovement != 0) {
+        //     return pos;
+        // }
+
+
+
+        var distance = Helper.getDistance3D(this.entity.getPosition(), this.snakeColliston.getLocalPosition());
+        if (distance < Helper.getDistance3D(pos, this.snakeColliston.getLocalPosition())) {
+            console.log("1");
+            return pos
+        } else {
+            pos.x -= xMovement
+            pos.z -= zMovement
+            return pos
+        }
+
+
     },
 
     rotate(direction) {
@@ -50,6 +81,14 @@ export const SnakeMove = Script.createScript({
         let angle = Math.atan2(direction.x, direction.z);
         angle = angle * 180 / Math.PI;
         this.entity.setEulerAngles(rot.x, angle - 90, rot.z);
+    },
+
+    setSnakeCollis(snake) {
+        this.snakeColliston = snake;
+    },
+
+    removeSnakeCollis() {
+        this.snakeColliston = null;
     },
 
     setVector(vec) {

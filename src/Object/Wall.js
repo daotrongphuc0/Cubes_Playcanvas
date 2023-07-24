3a01360f777afa22b89b06a6dbbb367a28de1b0c
daotrongphuc0 <phucdt504@gmail.com>
2023-07-24 15:58:10 +0700

    - chance expandAmount

--- src/Object/Wall.js
+++ src/Object/Wall.js
@@ -13,7 +13,7 @@ export class Wall extends Entity {
         this.setLocalScale(size.x, GameConstant.DEFAULT_HEIGHT_WALL, size.y)
 
         this.orientedBox = new OrientedBox(this.getWorldTransform());
-        const expandAmount = 0.06;
+        const expandAmount = 0.045;
         this.orientedBox.halfExtents.x += expandAmount;
         this.orientedBox.halfExtents.z += expandAmount;
 

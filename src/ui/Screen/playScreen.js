00587840c3046fed5468d80297adeba4247d7b20
daotrongphuc0 <phucdt504@gmail.com>
2023-07-27 10:58:16 +0700

    + event listener screen play

--- src/ui/Screen/playScreen.js
+++ src/ui/Screen/playScreen.js
@@ -36,28 +36,65 @@ export class PlayScreen extends UIScreen {
     });
     this.addChild(this.BgbtMove);
 
-    // this._initInputBtMove();
+    this.buttonPause = new ButtonPause({
+      anchor: new Vec4(0.98, 0.98, 0.98, 0.98),
+      pivot: new Vec2(1, 1),
+      margin: new Vec4(),
+    });
+    this.addChild(this.buttonPause);
+
+    this._initInputBtPause();
+    this._initInputBtMove();
+    this._initInputBtSpeed();
   }
 
-  // _initInputBtMove() {
-  //   this.BgbtMove.element.on('mousedown', this.onBtMoveDown, this)
-  //   this.BgbtMove.element.on('mousemove', this.onBtMoveMove, this)
-  //   this.BgbtMove.element.on('mouseup', this.onBtMoveUp, this)
-  //   this.BgbtMove.element.on('touchstart', this.onBtMoveDown, this)
-  //   this.BgbtMove.element.on('touchmove', this.onBtMoveMove, this)
-  //   this.BgbtMove.element.on('touchend', this.onBtMoveUp, this)
-  // }
+  _initInputBtMove() {
+    this.BgbtMove.element.on('mousedown', this.onBtMoveDown, this)
+    this.BgbtMove.element.on('mousemove', this.onBtMoveMove, this)
+    this.BgbtMove.element.on('mouseup', this.onBtMoveUp, this)
+    this.BgbtMove.element.on('touchstart', this.onBtMoveDown, this)
+    this.BgbtMove.element.on('touchmove', this.onBtMoveMove, this)
+    this.BgbtMove.element.on('touchend', this.onBtMoveUp, this)
+  }
+
+  _initInputBtSpeed() {
+    this.btSpeed.element.on('mousedown', this.onSpeedButtonDown, this)
+    this.btSpeed.element.on('mouseup', this.onSpeedButtonUp, this)
+    this.btSpeed.element.on('touchstart', this.onSpeedButtonDown, this)
+    this.btSpeed.element.on('touchend', this.onSpeedButtonUp, this)
+  }
+
+  _initInputBtPause() {
+    Util.registerOnTouch(this.buttonPause.element, this.onPauseButtonDown, this)
+  }
+
+  onPauseButtonDown() {
+    SceneManager.currentScene.gamePause()
+  }
+
+  onSpeedButtonDown() {
+    SceneManager.currentScene.player.activeAcceleration(GameConstant.PLAYER_SPEED_UP)
+  }
+  onSpeedButtonUp() {
+    SceneManager.currentScene.player.activeDeceleration(GameConstant.PLAYER_SPEED)
+  }
 
   onBtMoveDown(event) {
-    SceneManager.currentScene.onBtMoveDown(event)
+    this.touchedDown = true
+    this.downPos.x = event.x
+    this.downPos.y = event.y
   }
 
   onBtMoveMove(event) {
-    SceneManager.currentScene.onBtMoveMove(event)
+    if (this.touchedDown) {
+      SceneManager.currentScene.player.move.setVector(Helper.getVector(this.downPos.x, this.downPos.y, event.x, event.y))
+      this.setMove(Helper.getVector(this.downPos.x, this.downPos.y, event.x, event.y))
+    }
   }
 
   onBtMoveUp(event) {
-    SceneManager.currentScene.onBtMoveUp(event)
+    this.touchedDown = false
+    this.setDefault()
   }
 
   setMove(vector) {
